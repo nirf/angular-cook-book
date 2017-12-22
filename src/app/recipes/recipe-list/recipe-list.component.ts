d05a2c69b0efecd5ff0c180b33a8e16145ed44d8
Nir Fiegelshtein <fiegelshtein@gmail.com>
2017-12-22 18:53:20 +0200

    pass data from recipe-item to recipe-detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, EventEmitter, OnInit, Output } from '@angular/core'
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -11,8 +11,13 @@ export class RecipeListComponent implements OnInit {
     new Recipe('Fish', 'Sea Bream', 'http://cdni.condenast.co.uk/330x330/o_r/oct-07_malaysian-style-fish_b_330x330.jpg'),
     new Recipe('Stake', 'Entrecote', 'https://media-cdn.tripadvisor.com/media/photo-s/03/a7/da/b1/l-entrecote-steak-house.jpg')
   ]
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
   }
 
   ngOnInit() {
